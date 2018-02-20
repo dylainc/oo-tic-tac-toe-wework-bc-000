507f7d968c5b1879782b1d4c173a1f8843a9065c
Dylain Cohen <dylain.cohen@wework.com>
2018-02-20 00:29:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,10 @@ class TicTacToe
     !(board[index].nil? || board[index] == " ")
   end
 
+  def display_board(board)
+    puts board
+  end
+
   # Define your WIN_COMBINATIONS constant
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 

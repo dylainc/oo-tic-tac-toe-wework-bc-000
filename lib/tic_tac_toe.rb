558c6ea8be5f4de6fefce1243cfca6ce517f2ec7
Dylain Cohen <dylain.cohen@wework.com>
2018-02-20 00:32:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,118 +5,173 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def position_taken?(board, index)
-    !(board[index].nil? || board[index] == " ")
-  end
-
-  def display_board(board)
-    board
-  end
-
-  # Define your WIN_COMBINATIONS constant
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-  def won?(board)
-    WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-      position_1 = board[win_index_1] # load the value of the board at win_index_1
-      position_2 = board[win_index_2] # load the value of the board at win_index_2
-      position_3 = board[win_index_3] # load the value of the board at win_index_3
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
 
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        #true
-        did_win = true
-        return win_combination # return the win_combination indexes that won.
-      else
-        if win_combination == [2,4,6]
+def move(board, index, token)
+  board[index] = token
+end
 
-          #puts "last win combo test"
-        end
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
 
-        #puts "false section- win?"
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8) == true
+    true
+  else
+    false
+  end
+end
+
+def turn(board)
+  puts "Please select your position (1-9)"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+<<<<<<< HEAD
+    move(board, index, current_player(board))
+=======
+    move(board, index, "X")
+>>>>>>> 8fc3b0f63a4e1e1b9abe63ab6df7e48fa119ad96
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter = counter + 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      #true
+      did_win = true
+      return win_combination # return the win_combination indexes that won.
+    else
+      if win_combination == [2,4,6]
+
+        #puts "last win combo test"
       end
-    end
 
-    #false for empty board
-    if board.detect{|i| i == "X"}
-
-    elsif board.detect{|i| i == "O"}
-
-    else
-      false #empty board
-    end
-
-
-  end
-
-  #check for a draw
-  #if full?(board) == true
-  #  true
-  #else
-  #  false
-  #end
-
-
-  def full?(board)
-    if board.detect{|i| i == " "}
-      false
-    elsif board.detect{|i| i == ""}
-      false
-    else
-      true
+      #puts "false section- win?"
     end
   end
 
-  def draw?(board)
-    #puts full?(board)
-    #puts won?(board)
-    if full?(board) == true && (won?(board) == false || won?(board) == nil)
-      #puts "now testing won?=false"
+  #false for empty board
+  if board.detect{|i| i == "X"}
 
-        true
-        #puts "true section"
+  elsif board.detect{|i| i == "O"}
 
-      #puts "draw? section, full=true, won=false"
+  else
+    false #empty board
+  end
+end
+
+
+def full?(board)
+  if board.detect{|i| i == " "}
+    false
+  elsif board.detect{|i| i == ""}
+    false
+  else
+    true
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && (won?(board) == false || won?(board) == nil)
+    #puts board
+    #puts "draw called- true"
+    true
+  else
+    false
+  end
+#returns true if the board has not been won and is full
+#false if the board is not won and the board is not full
+#false if the board is won.
+
+end
+
+def over?(board)
+  if won?(board) == true || draw?(board) == true || full?(board) == true
+    true
+  elsif full?(board) == false && won?(board).is_a?(Array) == true
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board).is_a?(Array) == true
+    if board[won?(board)[0]] == "X"
+      "X"
     else
-      false
-      #puts "false section"
+      "O"
     end
-  #returns true if the board has not been won and is full
-  #false if the board is not won and the board is not full
-  #false if the board is won.
+  else
+    nil
+  end
+end
 
+def play(board)
+
+  until over?(board) == true do
+<<<<<<< HEAD
+    #input = gets
+=======
+    input = gets
+>>>>>>> 8fc3b0f63a4e1e1b9abe63ab6df7e48fa119ad96
+    turn(board)
   end
 
-  def over?(board)
-    if won?(board) == true || draw?(board) == true || full?(board) == true
-      true
-    elsif full?(board) == false && won?(board).is_a?(Array) == true
-      #puts board
-      #puts won?(board)
-      true
-    else
-      false
-    end
+  if won?(board).is_a?(Array) == true
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board) == true
+    puts "Cat's Game!"
   end
 
-  def winner(board)
-    puts "test"
-    if won?(board).is_a?(Array) == true
-      puts won?(board)
-      puts won?(board)[0]
-      if board[won?(board)[0]] == "X"
-        "X"
-      else
-        "O"
-      end
-    else
-      nil
-    end
-
-  end
+end
 
 
 
